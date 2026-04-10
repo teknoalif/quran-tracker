da73e5944eb8d1602dfbdc9c8db23fdc56c2b3f1
azharfa99 <azharfaturohman29@gmail.com>
2026-04-11 06:21:09 +0700

    Fix 404: Update routing and allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-veb2^qdh9ws6zytj&acfuq2%-ldn_nrg63efn3f)p5%4vxe(x%'
 DEBUG = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['qurantracker.kakalif.my.id', '.vercel.app', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'monitoring',
