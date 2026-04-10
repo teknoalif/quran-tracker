498d1e795f6b1ff67e110e11ef0a6897e8fcdafe
teknoalif <email-github-bapak@example.com>
2026-04-11 06:48:21 +0700

    Fix: Add auto-migration to wsgi

--- core/wsgi.py
+++ core/wsgi.py
@@ -2,6 +2,11 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quran_tracker.settings')
+from django.core.management import call_command
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f'Migration failed: {e}')
 application = get_wsgi_application()
 
 try:
